86e3c7d92807f9cd6e29971795b531dcc6bb44a6
Nick Thompson <nick.thompson@raizlabs.com>
2016-09-01 12:44:42 -0400

    Update jscodeshift transform to replace and rewrite style expr

--- transforms/iss.js
+++ transforms/iss.js
@@ -1,18 +1,27 @@
-/**
- * This replaces every occurence of variable "foo".
- */
 module.exports = function(fileInfo, api) {
   const j = api.jscodeshift;
   const root = j(fileInfo.source);
 
-  const callExpressions = root.find(j.CallExpression, {
+  root.find(j.CallExpression, {
     callee: {
       type: 'Identifier',
       name: '__issStyleSheetCreate__',
     }
-  }).filter(p => p.value.arguments.length == 1 && p.value.arguments[0].type === 'ObjectExpression');
+  }).filter(function(expr) {
+    const args = expr.value.arguments;
+    return args.length === 1 && args[0].type === 'ObjectExpression';
+  }).replaceWith(function(expr) {
+    const styleExpr = expr.value.arguments[0];
 
-  callExpressions.replaceWith(p => p.value.arguments[0]);
+    const props = styleExpr.properties.map(function(property) {
+      // This is what we write out to a stylesheet, with a selector governed
+      // by the same name we pass as the literal value below.
+      console.log(j(property.value).toSource());
+      return j.property('init', property.key, j.literal('swag'));
+    });
+
+    return j.objectExpression(props);
+  });
 
   return root.toSource();
 };
