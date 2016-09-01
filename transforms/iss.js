ea44330b3136c785ed039ea055130d3ea366ab48
Nick Thompson <nick.thompson@raizlabs.com>
2016-09-01 17:11:44 -0400

    Hacky, working first pass at css output

--- transforms/iss.js
+++ transforms/iss.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+const CSSPropertyOperations = require('react/lib/CSSPropertyOperations');
+
 module.exports = function(fileInfo, api) {
   const j = api.jscodeshift;
   const root = j(fileInfo.source);
@@ -12,14 +16,22 @@ module.exports = function(fileInfo, api) {
     return args.length === 1 && args[0].type === 'ObjectExpression';
   }).replaceWith(function(expr) {
     const styleExpr = expr.value.arguments[0];
+    const batch = [];
 
     const props = styleExpr.properties.map(function(property) {
       // This is what we write out to a stylesheet, with a selector governed
       // by the same name we pass as the literal value below.
-      console.log(j(property.value).toSource());
-      return j.property('init', property.key, j.literal('swag'));
+      const stylesObj = JSON.parse(j(property.value).toSource());
+      const markup = CSSPropertyOperations.createMarkupForStyles(stylesObj, null);
+      const selector = 'swag';
+      const declaration = `.${selector} {\n${markup}\n}`
+
+      batch.push(declaration);
+      return j.property('init', property.key, j.literal(selector));
     });
 
+    const output = batch.join('\n');
+    fs.appendFileSync('./out/iss.css', output);
     return j.objectExpression(props);
   });
 
