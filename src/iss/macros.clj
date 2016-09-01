16eae1b3ce5cd66cc5718495c97c2495009047e3
Nick Thompson <nick.thompson@raizlabs.com>
2016-09-01 12:07:46 -0400

    Expected behavior for defstyles macro

--- src/iss/macros.clj
+++ src/iss/macros.clj
@@ -1,6 +1,6 @@
 (ns iss.macros
-  (:require [iss.constants]
-             [cljs.core]))
+  (:require [iss.constants])
+  (:import [cljs.tagged_literals JSValue]))
 
 ; (defmacro inline [x] (var-get (ns-resolve 'iss.constants x)))
 
@@ -9,19 +9,28 @@
   TODO: GOtta watch out for values which resolve nil in ns-resolve.. like literals"
   (reduce-kv (fn [acc k v]
                (if (map? v)
-                 (assoc acc k (inline v))
+                 (assoc acc k (JSValue. (inline v)))
                  (assoc acc k (var-get (ns-resolve 'iss.constants v))))) {} m))
 
-(defn map->js-obj [x]
-  (when-not (nil? x)
-    (cond
-      (map? x) (let [m (cljs.core/js-obj)]
-                 (doseq [[k v] x]
-                   (aset m (name x) (map->js-obj v)))
-                 m)
-      :else x)))
+;(defn map->js-obj [x]
+;  (if (map? x)
+;    `(cljs.core/js-obj ~@(mapcat (fn [[k v]] [(name k) (map->js-obj v)]) x))
+;    x))
+
+;(defn map->js [x]
+;  (list 'js* "~{}" (json/write-str x)))
+
+;(defmacro defstyles [label styles]
+;  (let [swag (walk/postwalk-demo (.-val styles))]
+;    `(def ~label ~styles)))
+;        sty (inline styles)
+;        jsform (map->js-obj sty)]
+;    `(def ~label (js/__issStyleSheetCreate__ ~jsform))))
 
 (defmacro defstyles [label styles]
-  (let [sty (inline styles)
-        jsform (map->js-obj sty)]
-    `(def ~label ~sty)))
+  (let [inlined (JSValue. (inline styles))]
+    `(def ~label ~inlined)))
+
+(defmacro defclass [label styles]
+  (let [inlined (JSValue. (inline (.-val styles)))]
+    `(def ~label ~inlined)))
