eb66d4ac1baeda6ba83040f9b85a447f7042887f
Nick Thompson <nick.thompson@raizlabs.com>
2016-09-02 15:29:59 -0400

    Remove mount hooks

--- src/iss/core.cljs
+++ src/iss/core.cljs
@@ -40,12 +40,6 @@
 (defmethod even-odd-widget true
   [props owner]
   (reify
-    om/IWillMount
-    (will-mount [_]
-      (println "Even widget mounting"))
-    om/IWillUnmount
-    (will-unmount [_]
-      (println "Even widget unmounting"))
     om/IRender
     (render [_]
       (dom/div #js {:className (.-widget even-odd)}
@@ -58,12 +52,6 @@
 (defmethod even-odd-widget false
   [props owner]
   (reify
-    om/IWillMount
-    (will-mount [_]
-      (println "Odd widget mounting"))
-    om/IWillUnmount
-    (will-unmount [_]
-      (println "Odd widget unmounting"))
     om/IRender
     (render [_]
       (dom/div #js {:className (.-widget even-odd)}
